5da053ab1b8cd18b588b05cd2ee84df833ee3bdc
ErlensGadzans  <erlens.gadzans@gmail.com>
2022-04-28 12:45:50 +0300

    Task Increment and Decrement

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="app">
+    <MyComponent />
+  </div>
+</template>
+
+<script >
+import MyComponent from "./components/MyComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    MyComponent,
+  },
+};
+</script>
+
---
+++ src/components/MyComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="root">{{ text }}</div>
+  <div class="input-container">
+    <button @click="decreaseNumber">-</button>
+    <input type="number" :value="number" :min="minNumber" :max="maxNumber" />
+    <button @click="inceaseNumber">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MyComponent",
+  data() {
+    return {
+      text: "Choose amount of products",
+      minNumber: 1,
+      maxNumber: 10,
+      number: 1,
+    };
+  },
+  methods: {
+    inceaseNumber() {
+      if (this.number < this.maxNumber) {
+        this.number++;
+      }
+    },
+    decreaseNumber() {
+      if (this.number > this.minNumber) {
+        this.number--;
+      }
+    },
+  },
+};
+</script>
